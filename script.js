263178dcf91c6b6b0bcb77b48d3e8fdec643e5d9
Simplyj777 <jasmine.brown@pursuit.org>
2026-03-31 03:34:06 -0400

    Add life path calculation and report generation

---
+++ script.js
@@ -0,0 +1,19 @@
+function calculateLifePath(dob) {
+  const digits = dob.replaceAll("-", "").split("").map(Number);
+
+  let sum = digits.reduce((a, b) => a + b, 0);
+
+  while (sum > 9 && sum !== 11 && sum !== 22 && sum !== 33) {
+    sum = sum.toString().split("").reduce((a, b) => a + Number(b), 0);
+  }
+
+  return sum;
+}
+function generateReport() {
+  const dob = document.querySelector("input[type='date']").value;
+
+  const lifePath = calculateLifePath(dob);
+
+  alert("Your Life Path Number is: " + lifePath);
+}
+<button onclick="generateReport()">Generate My Full Bloom Report 🌸</button>
